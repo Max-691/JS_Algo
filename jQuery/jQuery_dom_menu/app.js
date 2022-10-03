ca89ba3a840fd362e9cb2043eebc80ca791e6d20
Max-691 <mcluzel@edenschool.fr>
2022-10-03 17:06:03 +0200

    Add exo

--- jQuery/jQuery_dom_menu/app.js
+++ jQuery/jQuery_dom_menu/app.js
@@ -1,6 +1,12 @@
 $("h2");
 $("h2").each(function () {
-    $("ul.menu").append($("<li></li>").append($("<a></a>").text($(this).text()).attr('href', `#${$(this).attr('id')}`)));
+    $("ul.menu").append($("<li>").append($("<a>").text($(this).text()).attr('href', `#${$(this).attr('id')}`)));
 
 });
-    
+
+// dès qu'on clique sur enfant de div le p apparait
+$('div').on('click', 'p', function(){
+ $(this).after('<p>Ce p a les mêmes caractéristiques que son parent</p>'); 
+});
+
+$("p").animate({opacity: '0.5'}, {easing})
