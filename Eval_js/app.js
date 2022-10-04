6522e3e4e392dc9e5703fac845b6fc227cac606d
Max-691 <mcluzel@edenschool.fr>
2022-10-04 15:29:42 +0200

    Add Eval after Olivier's review

--- Eval_js/app.js
+++ Eval_js/app.js
@@ -6,9 +6,9 @@ for (let i = 1; i < 10; i++) {
         const p = document.createElement('p');
         multiplication.appendChild(p);
         if(j === 3 || j === 6 || j === 9){
-            p.style.backgroundColor="blue";
+            p.style.backgroundColor="blue";           //Change background each 3 lines
         }
-        p.textContent = i + " x " + j + " = " + i*j
+        p.textContent = i + " x " + j + " = " + i * j //Example: 1 x 2 = 2
     }
 }
 
@@ -20,7 +20,7 @@ const prixFranc = 6.56;
 
 euro.addEventListener('keyup', ()=>{
     franc.value = parseFloat(euro.value * prixFranc);
-    
+    //User entre valeur en euro, resultat apparait dans champ Franc et inversement L25
 });
 franc.addEventListener('keyup', ()=>{
     euro.value = parseFloat(franc.value * prixEuro);
@@ -31,10 +31,16 @@ franc.addEventListener('keyup', ()=>{
 const list = document.querySelectorAll('#menu li');
 for (let i = 0; i < list.length; i++) {
    list[i].addEventListener('mouseover', ()=>{
-    list[i].style.color="red";
-    setTimeout(function(){
-        list[i].style.color="";
-    },400 )
-   })
-}
+    list[i].style.color = "red";
+})}
 
+//Liste panier
+ 
+const fruits = $('#fruits option');
+const panier = $('#panier option');
+const add = $('#add');
+const remove = $('#remove');
+
+for (let i = 0; i < fruits.length; i++) {
+    add.cli
+}
